d48f0ec9bd00b5d3ee4231ae73af8576d55f0407
aniket mishra <aniketmishra300@gmail.com>
2023-07-31 22:58:45 +0530

    Map & Set

---
+++ script.js
@@ -0,0 +1,16 @@
+// Question no. 1/////
+
+// let str = "abcadeecfb";
+
+// let a = str.split("");
+
+// let u = [...new Set(a)].join('');
+// console.log(u);
+
+
+
+// Question no.2//
+ for(let i=0; i<a ; i++)
+ {
+    
+ }
